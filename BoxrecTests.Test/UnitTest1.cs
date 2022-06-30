1f59c229ddaad21249a97b9fe2e5bbdc26c0c3d4
kacs <kacs@ailleron.com>
2022-06-30 15:43:43 +0200

    ADD GetBoxerFullName_Test

--- BoxrecTests.Test/UnitTest1.cs
+++ BoxrecTests.Test/UnitTest1.cs
@@ -1,4 +1,4 @@
-using boxrec;
+﻿using boxrec;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
@@ -62,7 +62,7 @@ public class UnitTest1
     }
 
     [Fact]
-    public void GetFightResult_Test()
+    public void GetWinner_Test()
     {
         var options = new DbContextOptionsBuilder<BoxrecContext>()
             .UseInMemoryDatabase(databaseName: "boxrec")
@@ -174,5 +174,30 @@ public class UnitTest1
             db.Database.EnsureDeleted();
         }
         
+    }
+
+    [Fact]
+    public void GetBoxerFullName_Test()
+    {
+        var options = new DbContextOptionsBuilder<BoxrecContext>()
+            .UseInMemoryDatabase(databaseName: "boxrec_test_record")
+            .Options;
+
+
+        // Insert seed data into the database using one instance of the db
+        using (var db = new BoxrecContext(options))
+        {
+            db.Boxers.Add(new Boxer { ID = 1, Name = "Paweł", Surname = "Jumper", DateOfBirth = System.DateTime.Today, Division_ID = 1 });
+            db.SaveChanges();
+        }
+
+        //Use a clean instance of the db to run the test
+        using (var db = new BoxrecContext(options))
+        {
+            var boxers = db.Boxers.ToList();
+            Assert.Equal("Paweł Jumper", $"{boxers[0].Name} {boxers[0].Surname}" );
+            db.Database.EnsureDeleted();
+        }
+
     }
 }
