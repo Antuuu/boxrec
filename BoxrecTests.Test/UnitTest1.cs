d8465858388b186a9082fa056ee796d03f8f9006
kacs <kacs@ailleron.com>
2022-06-30 11:52:53 +0200

    Add unit test AddBoxerWithUrl_Test

--- BoxrecTests.Test/UnitTest1.cs
+++ BoxrecTests.Test/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace BoxrecTests.Test;
 public class UnitTest1
 {
     [Fact]
-    public void GetAllTest()
+    public void AddBoxers_Test()
     {
         var options = new DbContextOptionsBuilder<BoxrecContext>()
             .UseInMemoryDatabase(databaseName: "boxrec")
@@ -32,6 +32,31 @@ public class UnitTest1
             List<Boxer> boxers = new List<Boxer>();
             boxers = context.Boxers.ToList();
             Assert.Equal(3, boxers.Count);
+            context.Database.EnsureDeleted();
+        }
+    }
+    [Fact]
+    public void AddBoxerWithUrl_Test()
+    {
+        var options = new DbContextOptionsBuilder<BoxrecContext>()
+            .UseInMemoryDatabase(databaseName: "boxrec")
+            .Options;
+
+
+        // Insert seed data into the database using one instance of the context
+        using (var context = new BoxrecContext(options))
+        {
+            context.Boxers.Add(new Boxer { ID = 1, Name = "Tyson", Surname = "Fury", DateOfBirth = System.DateTime.Today, Division_ID = 1, Photo_Url="https://to/img.png" });
+            context.SaveChanges();
+        }
+
+        //Use a clean instance of the context to run the test
+        using (var context = new BoxrecContext(options))
+        {
+            List<Boxer> boxers = new List<Boxer>();
+            boxers = context.Boxers.ToList();
+            Assert.Equal("https://to/img.png", boxers[0].Photo_Url);
+            context.Database.EnsureDeleted();
         }
     }
 }
